fb1ba507f355d7869ae1f65d366e666164239dab
izabala033 <iker13z@gmail.com>
2021-04-28 00:12:37 +0200

    postgres fix

--- postgres/create-config.sh
+++ postgres/create-config.sh
@@ -1,5 +1,5 @@
 kubectl create secret generic postgres-secret \
---from-literal=POSTGRES_USER="root" \
+--from-literal=POSTGRES_USER="postgres" \
 --from-literal=POSTGRES_PASSWORD="securepassword123"
 kubectl apply -f configmap.yaml
 kubectl apply -f postgres-service.yaml
